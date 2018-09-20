103d1fdcb610871bea291117f0fd962f4e6f17f3
Yulian-Caesar <yulik19971997@gmail.com>
2018-09-20 15:00:36 +0300

    added reviews

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
     console.log( "ready!" );
 });
+
+$('.reviews-slider').slick({
+	arrows: true,
+	dots: false,
+	infinite: true,
+	slidesToShow: 3,
+	centerMode: true,
+	slidesToScroll: 3,
+	variableWidth: true, 
+	responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        centerMode: false,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    ]
+})
